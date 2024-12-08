5108039726d25bc37952006403d23b38db40f359
Rodrigotna <rodrigo15tna@gmail.com>
2024-12-07 21:41:30 -0300

    Update 03_laboratorio.sql

--- tests/database/03_laboratorio.sql
+++ tests/database/03_laboratorio.sql
@@ -1,1 +1,27 @@
--- TODO: Adicionar registros de teste para a tabela de laboratorio
+USE lab_scheduler;
+
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 1, 1, 0);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 2, 2, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 3, 3, 0);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 4, 4, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 5, 5, 0);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 6, 6, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 7, 7, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 8, 8, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 9, 9, 0);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 10, 10, 0);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 11, 11, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 12, 12, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 13, 13, 0);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 14, 14, 1);
+INSERT INTO `lab_scheduler`.`ta_laboratorio_horario` (`id`, `id_laboratorio`, `id_horario`, `is_reservado`) VALUES (DEFAULT, 15, 15, 0);
+
+
+-- Seleciona todos os registros da tabela associativa
+SELECT * FROM `ta_laboratorio_horario`;
+
+-- Seleciona os laboratórios reservados (is_reservado = 1)
+SELECT * FROM `ta_laboratorio_horario` WHERE `is_reservado` = 1;
+
+-- Lista os horários associados a laboratórios que não estão reservados
+SELECT id, id_laboratorio, id_horario FROM ta_laboratorio_horario WHERE is_reservado = 0;
