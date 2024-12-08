9784563bcc905b5c16ca443d3dcecb1a7df3cf4b
Rodrigotna <rodrigo15tna@gmail.com>
2024-12-07 21:58:54 -0300

    Update 01_usuario.sql

--- tests/database/01_usuario.sql
+++ tests/database/01_usuario.sql
@@ -22,31 +22,5 @@ INSERT INTO `lab_scheduler`.`tb_usuario` (`id`, `nm_usuario`, `id_tipo_usuario`,
 INSERT INTO `lab_scheduler`.`tb_usuario` (`id`, `nm_usuario`, `id_tipo_usuario`, `ds_identificacao`) VALUES (DEFAULT, 'Mario Senna', 1, 'RA66666666');
 
 
--- Seleciona todos os tipos de usuário
-SELECT * FROM `td_tipo_usuario`;
 
--- Seleciona apenas o tipo de usuário 'Aluno'
-SELECT * FROM `td_tipo_usuario` WHERE `ds_tipo_usuario` = 'Aluno';
-
--- Conta quantos tipos de usuários existem na tabela
-SELECT COUNT(*) AS total_tipos FROM `td_tipo_usuario`;
-
--- Seleciona os tipos de usuários com IDs maiores que 1
-SELECT id, ds_tipo_usuario FROM td_tipo_usuario WHERE id > 1;
-
-
-
-
-
--- Seleciona todos os usuários
-SELECT * FROM `tb_usuario`;
-
--- Seleciona os usuários que são do tipo 'Aluno'
-SELECT `id`, `nm_usuario` FROM `tb_usuario` WHERE `id_tipo_usuario` = 1;
-
--- Seleciona o usuário com o ID 2
-SELECT * FROM `tb_usuario` WHERE `id` = 2;
-
--- Seleciona os usuários que possuem 'Ana' no nome
-SELECT id, nm_usuario, ds_identificacao FROM tb_usuario WHERE nm_usuario LIKE '%Mario%';
 
