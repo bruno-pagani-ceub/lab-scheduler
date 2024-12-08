c5804bcf78c444ce3e62d2d1e68efe4d314bcef7
Rodrigotna <rodrigo15tna@gmail.com>
2024-12-07 21:49:39 -0300

    Update 05_reserva.sql

--- tests/database/05_reserva.sql
+++ tests/database/05_reserva.sql
@@ -1,1 +1,31 @@
--- TODO: Adicionar registros de teste para a tabela de reserva
+USE lab_scheduler;
+
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'aula', 1, 1);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 0, 'manutencao', 2, 2);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'reuniao', 3, 3);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'evento', 4, 4);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 0, 'aula', 5, 5);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'aula', 6, 6);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'aula', 7, 7);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'aula', 8, 8);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 0, 'reuniao', 9, 9);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'reuniao', 10, 10);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'manutencao', 11, 11);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 0, 'aula', 12, 12);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 0, 'aula', 13, 13);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'evento', 14, 14);
+INSERT INTO `lab_scheduler`.`tb_reserva` (`id`, `is_ativa`, `tp_reserva`, `id_usuario`, `id_laboratorio_horario`) VALUES (DEFAULT, 1, 'aula', 15, 15);
+
+
+
+-- Seleciona todas as reservas
+SELECT * FROM `tb_reserva`;
+
+-- Seleciona as reservas ativas
+SELECT * FROM `tb_reserva` WHERE `is_ativa` = 1;
+
+-- Seleciona as reservas do tipo 'Aula'
+SELECT * FROM `tb_reserva` WHERE `tp_reserva` = 'Aula';
+
+-- Seleciona todas as reservas inativas
+SELECT id, tp_reserva, is_ativa FROM tb_reserva WHERE is_ativa = 0;
