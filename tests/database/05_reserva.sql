7e5f49ee75f7555526acac5b01dfbe71d0dfee79
“lucasmartins-92” <lucas-martins@outlook.com>
2024-12-11 23:56:14 -0300

    Bugfix: Corrected .sql files

--- tests/database/05_reserva.sql
+++ tests/database/05_reserva.sql
@@ -52,8 +52,6 @@ FROM ta_laboratorio_horario lh
 JOIN tb_laboratorio l ON lh.id_laboratorio = l.id
 JOIN tb_horario h ON lh.id_horario = h.id
 WHERE h.dt_dia = '2025-01-06'
-  AND h.hr_inicio = CAST('09:40' AS TIME) 
-  AND h.hr_fim = CAST('11:20' AS TIME)
   AND h.hr_inicio = CAST('11:30' AS TIME) 
   AND h.hr_fim = CAST('13:00' AS TIME)
   AND l.id = 4;
@@ -64,8 +62,6 @@ FROM ta_laboratorio_horario lh
 JOIN tb_laboratorio l ON lh.id_laboratorio = l.id
 JOIN tb_horario h ON lh.id_horario = h.id
 WHERE h.dt_dia = '2025-01-07'
-  AND h.hr_inicio = CAST('09:40' AS TIME) 
-  AND h.hr_fim = CAST('11:20' AS TIME)
   AND h.hr_inicio = CAST('11:30' AS TIME) 
   AND h.hr_fim = CAST('13:00' AS TIME)
   AND l.id = 4;
@@ -76,8 +72,6 @@ FROM ta_laboratorio_horario lh
 JOIN tb_laboratorio l ON lh.id_laboratorio = l.id
 JOIN tb_horario h ON lh.id_horario = h.id
 WHERE h.dt_dia = '2025-01-08'
-  AND h.hr_inicio = CAST('09:40' AS TIME) 
-  AND h.hr_fim = CAST('11:20' AS TIME)
   AND h.hr_inicio = CAST('11:30' AS TIME) 
   AND h.hr_fim = CAST('13:00' AS TIME)
   AND l.id = 4;
@@ -89,8 +83,6 @@ FROM ta_laboratorio_horario lh
 JOIN tb_laboratorio l ON lh.id_laboratorio = l.id
 JOIN tb_horario h ON lh.id_horario = h.id
 WHERE h.dt_dia = '2025-01-09'
-  AND h.hr_inicio = CAST('09:40' AS TIME) 
-  AND h.hr_fim = CAST('11:20' AS TIME)
   AND h.hr_inicio = CAST('11:30' AS TIME) 
   AND h.hr_fim = CAST('13:00' AS TIME)
   AND l.id = 4;
@@ -102,8 +94,6 @@ FROM ta_laboratorio_horario lh
 JOIN tb_laboratorio l ON lh.id_laboratorio = l.id
 JOIN tb_horario h ON lh.id_horario = h.id
 WHERE h.dt_dia = '2025-01-10'
-  AND h.hr_inicio = CAST('09:40' AS TIME) 
-  AND h.hr_fim = CAST('11:20' AS TIME)
   AND h.hr_inicio = CAST('11:30' AS TIME) 
   AND h.hr_fim = CAST('13:00' AS TIME)
   AND l.id = 4;
