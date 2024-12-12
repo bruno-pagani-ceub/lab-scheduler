b150fcd8ee76939e99728a2d2923399bf40949f0
antoniovna <50215885+antoniovna@users.noreply.github.com>
2024-12-11 22:24:27 -0300

    Feat: update the user controller to use on the user GUI

--- lab_scheduler/controllers/user_controller.py
+++ lab_scheduler/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from lab_scheduler.views import UserView, UserRegistrationView
+from lab_scheduler.views import UserView, UserRegistrationView, UpdateUserView
 from lab_scheduler.database.models import UserModel
 
 
@@ -9,15 +9,40 @@ class UserController:
         self.view = UserView(root, self)
         self.model = UserModel(db=db)
 
+    def load_users(self, nm_usuario):
+        return self.model.load_users(nm_usuario)
+
+    def delete_user(self, id):
+        return self.model.delete_user(id)
+
+    def register_user(self):
+        UserRegistrationController(self.root, self.db)
+
+    def update_users(self, id, nm_usuario, id_tipo_usuario, ds_identificacao):
+        UpdateUserController(
+            self.root, self.db, id, nm_usuario, id_tipo_usuario, ds_identificacao
+        )
+
+
 class UserRegistrationController:
     def __init__(self, root, db):
         self.root = root
         self.db = db
         self.view = UserRegistrationView(root, self)
-        
+        self.model = UserModel(db=db)
+
     def create_user(self, full_name, role, doc):
-        '''Inserts New User in Database.'''
         self.model.save_user(full_name, role, doc)
-        pass
-        
-    
+
+
+class UpdateUserController:
+    def __init__(self, root, db, id, nm_usuario, id_tipo_usuario, ds_identificacao):
+        self.root = root
+        self.db = db
+        self.model = UserModel(db=db)
+        self.view = UpdateUserView(
+            root, self, id, nm_usuario, id_tipo_usuario, ds_identificacao
+        )
+
+    def update_user(self, id, nm_usuario, id_tipo_usuario, ds_identificacao):
+        self.model.update_user(id, nm_usuario, id_tipo_usuario, ds_identificacao)
