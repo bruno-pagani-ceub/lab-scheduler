ea738cdaf1d69a01a6d435050649a03ce2c68de4
Lucas Martins de Andrade <61472844+lucasmartins-92@users.noreply.github.com>
2024-12-12 14:21:45 -0300

    feat: single and recurrent reservation (#17)
    * feat: single reservation, GUI WIP: recurrent reservation
    
    * fix: correct insertions
    
    * fix: do not use variables
    
    * fix: leave only necessary
    
    * feature: add single or recurrent reservation
    
    * reserve_lab() no LabReservationController agora invoca LabReservationController
    
    * FEATURE: Add reservation search
    BUGFIX: recurrent reservations now work properly
    
    * Docs: add "cod_reserva" na modelagem
    
    * fix: fix sql.insert_many (#30)
    
    * fix: fix sql.insert_many
    
    * fix: remove unused parameter
    
    * Bugfixes, updated available times search for recurrent reservations
    
    * Implementação da GUI do usuário (#31)
    
    * Feat: update the user controller to use on the user GUI
    
    * Feat: updating the user modal to use the new functions in the user GUI
    
    * Feat: Updating the user GUI to have CRUD functions
    
    * Bugfix: Corrected .sql files
    
    * Small fixes

--- lab_scheduler/controllers/reservation_controller.py
+++ lab_scheduler/controllers/reservation_controller.py
@@ -1,18 +1,85 @@
-from lab_scheduler.views import ReservationView, LabReservationView
-from lab_scheduler.database.models import ReservationModel
+from lab_scheduler.views import ReservationView, LabReservationView, ViewReservationView
+from lab_scheduler.database.models import ReservationModel, ViewReservationModel, LabReservationModel
+import datetime
+from lab_scheduler import static
 
 
 class ReservationController:
     def __init__(self, root, db):
         self.root = root
         self.db = db
-        self.view = ReservationView(root, self)
         self.model = ReservationModel(db=db)
+        self.view = ReservationView(root, self)
+
+    def manage_reservations(self):
+        ViewReservationController(self.root, self.db)
+
+    def reserve_lab(self):
+        LabReservationController(self.root, self.db)
+
 
 class LabReservationController:
     def __init__(self, root, db):
         self.root = root
         self.db = db
+        self.model = LabReservationModel(db=db)
         self.view = LabReservationView(root, self)
-        
+
+    def get_user_data(self, user_id):
+        result = self.model.get_user(user_id)
+        if result:
+            return result
+        else:
+            return ""
+
+    def retrieve_lab(self):
+        lab_list = self.model.get_lab_list()
+        return lab_list
+
+    def convert(self, timeslots):
+        for key, value in timeslots.items():
+            if key not in ["hr_inicio", "hr_fim"]:
+                continue
+            total_seconds = value.total_seconds()
+            hours = int(total_seconds // 3600)
+            minutes = int((total_seconds % 3600) // 60)
+            timeslots[key] = f"{hours:02}:{minutes:02}"
+        return timeslots
+
+    def get_available_timeslots(self, date, lab):
+        datetime_date = datetime.datetime.strptime(date, "%d/%m/%Y")
+        adjusted_date = datetime_date.strftime("%Y-%m-%d")
+        return self.model.get_available_timeslots(
+            adjusted_date, lab
+        )
+
     
+    def get_timeslots_weekday(self, lab, weekday, semester, year):
+        return self.model.get_available_timeslots_weekday(
+            lab, weekday, semester, year
+        )
+
+    def submit_single_reservation(self, selected_items):
+        self.model.make_single_reservation(selected_items["user"], selected_items["type"],
+                                           selected_items["lab"], selected_items["timeslot"])
+
+    def submit_recurrent_reservation(self, selected_items):
+        success_check = self.model.make_recurrent_reservation(
+            selected_items["user"], selected_items["type"], selected_items["lab"], selected_items["weekday"],
+            selected_items["timeslot"], selected_items["semester"], selected_items["year"]
+        )
+        if success_check:
+            return True
+        else:
+            return False
+
+class ViewReservationController:
+    def __init__(self, root, db):
+        self.root = root
+        self.db = db
+        self.model = ViewReservationModel(db=db)
+        self.view = ViewReservationView(root, self)
+
+    def search_all(self):
+        all_list = self.model.search_all()
+        return all_list
--- lab_scheduler/database/models/__init__.py
+++ lab_scheduler/database/models/__init__.py
@@ -3,6 +3,8 @@ from lab_scheduler.database.models.user_model import UserModel
 from lab_scheduler.database.models.timeslots_model import TimeSlotsModel
 from lab_scheduler.database.models.lab_model import LabModel
 from lab_scheduler.database.models.reservation_model import ReservationModel
+from lab_scheduler.database.models.reservation_model import LabReservationModel
+from lab_scheduler.database.models.reservation_model import ViewReservationModel
 from lab_scheduler.database.models.report_model import ReportModel
 
 __all__ = [
@@ -11,5 +13,7 @@ __all__ = [
     "LabModel",
     "TimeSlotsModel",
     "ReservationModel",
+    "LabReservationModel",
+    "ViewReservationModel",
     "ReportModel",
 ]
