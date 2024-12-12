f0701e330a9e8ebe9c56f7fa09dfd048bf215ec2
antoniovna <50215885+antoniovna@users.noreply.github.com>
2024-12-11 22:25:11 -0300

    Feat: updating the user modal to use the new functions in the user GUI

--- lab_scheduler/database/models/user_model.py
+++ lab_scheduler/database/models/user_model.py
@@ -1,8 +1,78 @@
 from lab_scheduler.database.sql import SQL
 
+
 class UserModel:
     def __init__(self, db):
         self.db: SQL = db
-        
+
     def save_user(self, full_name, role, doc):
-        pass
+        user_data = {
+            "nm_usuario": full_name,
+            "id_tipo_usuario": role,
+            "ds_identificacao": doc,
+        }
+
+        insert_query = """
+           INSERT INTO tb_usuario (nm_usuario, id_tipo_usuario, ds_identificacao)
+           VALUES (%s, %s, %s)
+           """
+
+        params = (
+            user_data["nm_usuario"],
+            user_data["id_tipo_usuario"],
+            user_data["ds_identificacao"],
+        )
+
+        return self.db.insert(insert_query, params)
+
+    def update_user(self, id, full_name, role, doc):
+        user_data = {
+            "id": id,
+            "nm_usuario": full_name,
+            "id_tipo_usuario": role,
+            "ds_identificacao": doc
+
+        }
+
+        update_query = """UPDATE tb_usuario
+                SET nm_usuario = %s,
+                    id_tipo_usuario = %s,
+                    ds_identificacao = %s
+                WHERE id = %s;"""
+
+        params = (
+            user_data["nm_usuario"],
+            user_data["id_tipo_usuario"],
+            user_data["ds_identificacao"],
+            user_data["id"]
+        )
+        print("veio aqui 4")
+        self.db.insert(update_query, params)
+
+    def load_users(self, nm_usuario):
+        load_query = """
+            SELECT * FROM tb_usuario
+        """
+        params = []
+
+        # Add condition if nm_usuario is provided
+        if nm_usuario:
+            load_query = """
+                SELECT * FROM tb_usuario WHERE NM_USUARIO LIKE %s
+            """
+            # Use % for partial matching in LIKE
+            params = (f"%{nm_usuario}%",)
+
+        return self.db.get_list(load_query, params)
+
+    def delete_user(self, id):
+        print("delete id -> ", id)
+        delete_query = """
+           DELETE FROM tb_usuario WHERE ID = %s
+           """
+
+        params = (
+            id,
+        )
+
+        return self.db.upd_del(delete_query, params)
