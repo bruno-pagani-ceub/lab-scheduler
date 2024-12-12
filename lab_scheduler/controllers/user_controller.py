9baf50b73a162073ee9387717cbdc5ff8a2cb3b8
antoniovna <50215885+antoniovna@users.noreply.github.com>
2024-12-11 23:47:36 -0300

    Implementação da GUI do usuário (#31)
    * Feat: update the user controller to use on the user GUI
    
    * Feat: updating the user modal to use the new functions in the user GUI
    
    * Feat: Updating the user GUI to have CRUD functions

--- lab_scheduler/controllers/user_controller.py
+++ lab_scheduler/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from lab_scheduler.views import UserView, UserRegistrationView
+from lab_scheduler.views import UserView, UserRegistrationView, UpdateUserView
 from lab_scheduler.database.models import UserModel
 
 
@@ -6,18 +6,47 @@ class UserController:
     def __init__(self, root, db):
         self.root = root
         self.db = db
-        self.view = UserView(root, self)
         self.model = UserModel(db=db)
+        self.view = UserView(root, self)
+
+    def load_users(self, nm_usuario):
+        return self.model.load_users(nm_usuario)
+
+    def delete_user(self, id):
+        return self.model.delete_user(id)
+
+    def register_user(self):
+        UserRegistrationController(self.root, self.db)
+
+    def update_users(self, id, nm_usuario, id_tipo_usuario, ds_identificacao):
+        UpdateUserController(
+            self.root, self.db, id, nm_usuario, id_tipo_usuario, ds_identificacao
+        )
+
 
 class UserRegistrationController:
     def __init__(self, root, db):
         self.root = root
         self.db = db
+        self.model = UserModel(db=db)
         self.view = UserRegistrationView(root, self)
-        
+
     def create_user(self, full_name, role, doc):
-        '''Inserts New User in Database.'''
         self.model.save_user(full_name, role, doc)
-        pass
         
-    
+    def get_positions(self):
+        result = self.model.get_positions()
+        return {line["ds_tipo_usuario"] : line["id"] for line in result}
+
+
+class UpdateUserController:
+    def __init__(self, root, db, id, nm_usuario, id_tipo_usuario, ds_identificacao):
+        self.root = root
+        self.db = db
+        self.model = UserModel(db=db)
+        self.view = UpdateUserView(
+            root, self, id, nm_usuario, id_tipo_usuario, ds_identificacao
+        )
+
+    def update_user(self, id, nm_usuario, id_tipo_usuario, ds_identificacao):
+        self.model.update_user(id, nm_usuario, id_tipo_usuario, ds_identificacao)
