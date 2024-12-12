40a6c0de73e612da1632daaa3bd8cfc14bb01eea
Rodrigotna <rodrigo15tna@gmail.com>
2024-12-12 11:39:01 -0300

    Update reports_controller.py

--- lab_scheduler/controllers/reports_controller.py
+++ lab_scheduler/controllers/reports_controller.py
@@ -1,20 +1,18 @@
-from lab_scheduler.database.models import ReportModel
-from lab_scheduler.services.weekly_report import Folder
-from lab_scheduler.views import ScheduleGenerationView
-
+from tkinter import messagebox
+from lab_scheduler.database.models.report_model import ReportModel
+from lab_scheduler.views.report_gen_view import ScheduleGenerationView
 
 class ScheduleGenerationController:
     def __init__(self, root, db):
         self.root = root
-        self.db = db
-        self.view = ScheduleGenerationView(root, self)
-        self.report_generator = Folder()
-        self.model = ReportModel(db=db)
-        
-    def _get_week_schedule_data(self): 
-        pass
+        self.model = ReportModel(db)
+        self.open_schedule_view()
 
-    def generate_report(self):
-        data = self._get_week_schedule_data()
-        self.report_generator.generate_report(data)
-        pass
+    def open_schedule_view(self):
+        try:
+            schedule_data = self.model.get_weekly_schedule()
+            ScheduleGenerationView(self.root, schedule_data)
+        except Exception as e:
+            messagebox.showerror("Erro", f"Falha ao carregar o cronograma: {e}")
+
+
